8dfb1dde05c72415547d560bb02807cce59bc045
di-Timonix <97761906+di-Timonix@users.noreply.github.com>
2022-02-21 13:59:31 +0100

    2.0
    Signed-off-by: di-Timonix <97761906+di-Timonix@users.noreply.github.com>

--- bm-jezyk.php
+++ bm-jezyk.php
@@ -2,8 +2,7 @@
 /*
 	CMS ,,Black Min''  Został stworzony przez di_Timonix'a
 	
-	ten plik służy do załadowanie cms
-	ładowanie wszystkch elemtetów bm i pokazywanie błędów jeżeli są takie
+	ten plik służy do ładowania tłumaczeniami
 	
 	Black Min cms,
 	
