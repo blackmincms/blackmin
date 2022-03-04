1dfd6523738b38942e098e8a7f07b28afc4bd482
di-Timonix <97761906+di-Timonix@users.noreply.github.com>
2022-03-04 23:57:11 +0100

    2.0
    Signed-off-by: di-Timonix <97761906+di-Timonix@users.noreply.github.com>

--- bm/admin/insert/add-uzytkownik.php
+++ bm/admin/insert/add-uzytkownik.php
@@ -6,15 +6,12 @@
 	
 	Black Min cms,
 	
-	#plik: 1.1
+	#plik: 2.0
 */
 				
 	// ładowanie jądra black mina
 	require_once "../black-min.php";
 	
-	
-	require_once "../../../connect.php";
-	require_once "../laduj/class-get-ustawienia.php";
 	// Tworzenie nowej klasy i wywołanie jej w celu pobrania ustawień Black Mina
 	$url_serwera_bm = BM_SETTINGS["url_server"];
 	
@@ -22,6 +19,8 @@
 	
 	if(isset($_POST['nick']))
 	{
+		global $db_bm;
+
 		// pobieranie danych metodą post
 		
 		$nick = $_POST['nick'];
@@ -39,14 +38,14 @@
 
 		// filtrowanie danych dla pewnośći
 		
-		$nick = html_entity_decode($nick, ENT_QUOTES, "UTF-8");
-		$imie = html_entity_decode($imie, ENT_QUOTES, "UTF-8");
-		$nazwisko = html_entity_decode($nazwisko, ENT_QUOTES, "UTF-8");
-		$mail = html_entity_decode($mail, ENT_QUOTES, "UTF-8");
-		$plec = html_entity_decode($plec, ENT_QUOTES, "UTF-8");
-		$haslo = html_entity_decode($haslo, ENT_QUOTES, "UTF-8");
-		$haslo2 = html_entity_decode($haslo2, ENT_QUOTES, "UTF-8");
-		$rola = html_entity_decode($rola, ENT_QUOTES, "UTF-8");
+		$nick = $db_bm->valid($nick);
+		$imie = $db_bm->valid($imie);
+		$nazwisko = $db_bm->valid($nazwisko);
+		$mail = $db_bm->valid($mail);
+		$plec = $db_bm->valid($plec);
+		$haslo = $db_bm->valid($haslo);
+		$haslo2 = $db_bm->valid($haslo2);
+		$rola = $db_bm->valid($rola);
 		
 		$wszystko_ok = true;
 		
@@ -55,6 +54,7 @@
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> Nick musi posiadać od 3 do 24 znaków! </section>';
+			exit();
 		}
 		
 		// imie i naazwisko
@@ -64,6 +64,7 @@
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> imie musi posiadać od 3 do 24 znaków! </section>';
+			exit();
 		}
 	 
 		//Sprawdź poprawność nazwiska
@@ -71,6 +72,7 @@
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> nazwisko musi posiadać od 4 do 34 znaków! </section>';
+			exit();
 		}
 		
 		
@@ -83,6 +85,7 @@
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> Podaj poprawny adres e-mail! </section>';
+			exit();
 		}
 		
 		//Sprawdź poprawność hasła
@@ -90,12 +93,14 @@
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> Hasło musi posiadać od 8 do 30 znaków! </section>';
+			exit();
 		}
 		
 		if ($haslo!=$haslo2)
 		{
 			$wszystko_ok = false;
 			echo '<section class="tsr tsr-alert tsr-alert-error"> Podane hasła nie są identyczne! </section>';
+			exit();
 		}	
 
 		$haslo_hash = password_hash($haslo, PASSWORD_DEFAULT);
@@ -127,81 +132,35 @@
 			$flaga = 30;
 		}	
 		
-		// jęzeli jeżeli walidacja przeszła pomyśnie to otwiera się połączenie z bazą danych
-			
-		mysqli_report(MYSQLI_REPORT_STRICT);
-		
-		try 
-		{
-			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-			if ($polaczenie->connect_errno!=0)
-			{
-				throw new Exception(mysqli_connect_errno());
+		if ($out = $db_bm->query("SELECT `id` FROM `|prefix|bm_uzytkownicy` WHERE email='$mail'")) {
+			if ($out["num_rows"] > 0) {
+				$wszystko_ok = false;
+				echo '<section class="tsr tsr-alert tsr-alert-error"> Istnieje już konto przypisane do tego adresu e-mail! </section>';
+				exit();
 			}
-			else
-			{
-				
-				mysqli_query($polaczenie, "SET CHARSET utf8");
-				mysqli_query($polaczenie, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");	
-
-				//Czy email już istnieje?
-				$rezultat = $polaczenie->query("SELECT `id` FROM `".$prefix_table."bm_uzytkownicy` WHERE email='$mail'");
-				
-				if (!$rezultat) throw new Exception($polaczenie->error);
-				
-				$ile_takich_maili = $rezultat->num_rows;
-				if($ile_takich_maili>0)
-				{
-					$wszystko_ok = false;
-					echo '<section class="tsr tsr-alert tsr-alert-error"> Istnieje już konto przypisane do tego adresu e-mail! </section>';
-				}		
-
-				//Czy nick jest już zarezerwowany?
-				$rezultat2 = $polaczenie->query("SELECT `id` FROM `".$prefix_table."bm_uzytkownicy` WHERE nick='$nick'");
-				
-				if (!$rezultat2) throw new Exception($polaczenie->error);
-				
-				$ile_takich_nickow = $rezultat2->num_rows;
-				if($ile_takich_nickow>0)
-				{
-					$wszystko_ok = false;
-					echo '<section class="tsr tsr-alert tsr-alert-error"> Istnieje już konto o takim nicku! Wybierz inne. </section>';
-				}
-				
-				// jeżeli wszystkie testy walidacyjne przeszły pomyśnie to dodajemy nowy post
-				// wyśiwtlamy informacje dla użytkownika że post został dodany do bazy danych
-				
-				if ($wszystko_ok == true)
-				{
-					//Hurra, wszystkie testy zaliczone, dodajemy gracza do bazy
-					
-					if ($polaczenie->query("INSERT INTO `".$prefix_table."bm_uzytkownicy` VALUES (NULL, '$nick', '$imie', '$nazwisko', '$mail', '$plec', '$datetime', '$url_serwera_bm"."pliki/logo/logo_bm_white_2_100_100.png', '$haslo_hash', '$token', 'aktywacja_konta', '$rola', '$flaga', 'ofline', '$datetime', '[test_system_users],[test_mesages]')"))
-					{
-						echo '<section class="tsr-alert tsr-alert-success"> Nowy Użytkownik Został Dodany Poprawnie </section>';
-						echo '<script type="text/javascript">
-								$("input,textarea").each(function(){
-								$(this).val("");
-								});
-							</script>
-							<meta http-equiv="refresh" content="0">
-						';
-					}
-					else
-					{
-						throw new Exception($polaczenie->error);
-					}
-					
-				}
-				
-				$polaczenie->close();
-			}
-			
 		}
-		
-          
-		catch(Exception $e)
-		{
-			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o powtórzenie działań w innym terminie!</span>';
+		if ($out = $db_bm->query("SELECT `id` FROM `|prefix|bm_uzytkownicy` WHERE nick='$nick'")) {
+			if ($out["num_rows"] > 0) {
+				$wszystko_ok = false;
+				echo '<section class="tsr tsr-alert tsr-alert-error"> Istnieje już konto o takim nicku! Wybierz inne. </section>';
+				exit();
+			}
+		}
+
+		if ($wszystko_ok != false) {
+			if ($out = $db_bm->insert("INSERT INTO `|prefix|bm_uzytkownicy` VALUES (NULL, '$nick', '$imie', '$nazwisko', '$mail', '$plec', '$datetime', '$url_serwera_bm"."pliki/logo/logo_bm_white_2_100_100.png', '$haslo_hash', '$token', 'aktywacja_konta', '$rola', '$flaga', 'ofline', '$datetime', '[test_system_users],[test_mesages]')")) {
+				echo '<section class="tsr-alert tsr-alert-success"> Nowy Użytkownik Został Dodany Poprawnie </section>';
+				echo '<script type="text/javascript">
+						$("input,textarea").each(function(){
+						$(this).val("");
+						});
+					</script>
+					<meta http-equiv="refresh" content="0">
+				';
+			}
+		} else {
+			echo '<section class="tsr-alert tsr-alert-error"> Wystąpił nieznany błąd! </section>';
+			exit();
 		}			
 		
 	}
